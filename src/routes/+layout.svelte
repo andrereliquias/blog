f705ebbe32a61fdca39bb20c11ae36d02927993b
AndreReliquias <andrereliquias.contato@gmail.com>
2024-09-01 23:12:13 -0300

    feat: add tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "../app.css";
+</script>
+
+<div>
+	<header>
+    <p>this is the header</p>
+  </header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>this is the footer</p>
+	</footer>
+</div>
