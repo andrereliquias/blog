272f619104e025fe34977ae48c7562879e773978
AndreReliquias <andrereliquias.contato@gmail.com>
2024-09-26 22:54:18 -0300

    feat: update page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Header from './Header.svelte';
 </script>
 
-<div class="flex flex-col mx-auto w-64 md:w-5/12 text-base gap-y-8 text-justify my-10">
+<div class="flex flex-col mx-auto w-72 md:w-5/12 text-base gap-y-8 text-justify my-10">
 	<Header />
 
 	<main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,14 @@
 	<h1>Hey there 👋</h1>
 	<p>
 		I'm a 23-year-old computer engineer based in Brazil. I've been working as a software engineer
-		for over 3 years. I'm currently studying a master's degree in computer science and
-		computational mathematics, focusing the research on time series data mining. I'm also doing an
-		MBA in software engineering. If you want to reach out, feel free to contact me through my email:
-		<a href="mailto:hi@minari.xyz">hi@minari.xyz</a>.
+		for over 3 years. I'm currently studying a master's degree in computer science and computational
+		mathematics, focusing the research on time series data mining. I'm also doing an MBA in software
+		engineering. If you want to reach out, feel free to contact me through my email:
+		<a href="mailto:hi@minari.xyz">hi@minari.xyz</a> or my <a href="https://www.linkedin.com/in/andrerminari/">/in</a>.
 	</p>
 	<p>
-		I created this page to share notes about anything that I like. I'm currently interested in
-		databases, data mining, complex networks and compilers. Once in a while, I get drawn to quantum
-		computing. Don't be surprised if you find other non-tech-related stuff here.
+		I created this page to share notes about anything that I like. Don't be surprised if you find
+		other non-tech-related stuff here.
 	</p>
 	<p>
 		Check out my <a href="/now">/now</a> page to see what I'm doing right now.
