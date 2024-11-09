1bb00bf666f45641091df426b4593e7011c80426
AndreReliquias <andrereliquias.contato@gmail.com>
2024-11-09 18:24:06 -0300

    fix: buffer

--- src/routes/api/verify-mango-image/+server.ts
+++ src/routes/api/verify-mango-image/+server.ts
@@ -1,9 +1,7 @@
-// src/routes/classify/+server.ts
-
 import type { RequestHandler } from '@sveltejs/kit';
 import FormData from 'form-data';
 import axios from 'axios';
-import { Buffer } from 'buffer';
+import { Readable } from 'stream';
 
 export const POST: RequestHandler = async ({ request }) => {
   try {
@@ -17,12 +15,12 @@ export const POST: RequestHandler = async ({ request }) => {
 
     // Convert the File to a Buffer
     const arrayBuffer = await file.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
+		const buffer = Buffer.from(arrayBuffer);
+		const stream = Readable.from(buffer);
 
     // Create a new FormData instance
     const form = new FormData();
-    form.append('file', buffer, { filename: file.name, contentType: file.type });
-		console.log('form',  form)
+    form.append('file', stream, { filename: file.name, contentType: file.type });
     // Send the image to the FastAPI endpoint using axios
     const response = await axios.post('http://ec2-3-88-114-124.compute-1.amazonaws.com:8080/classify-mango', form, {
       headers: {
